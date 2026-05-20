cfd1cc8c46762becc302ef52e9959296feeb35aa
benjamin <bandasavanh.b@akira-sarl.fr>
2026-05-20 13:40:26 +0200

    [ADD] Ajout du bloc de gestion patient

---
+++ lib/features/patient/presentation/patient_state.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+import 'package:medicail/features/patient/domain/entities/patient.dart';
+
+sealed class PatientState extends Equatable {
+  const PatientState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class PatientInitial extends PatientState {
+  const PatientInitial();
+}
+
+final class PatientLoading extends PatientState {
+  const PatientLoading();
+}
+
+final class PatientLoaded extends PatientState {
+  const PatientLoaded(this.patients);
+
+  final List<Patient> patients;
+
+  @override
+  List<Object?> get props => [patients];
+}
+
+final class PatientFailure extends PatientState {
+  const PatientFailure(this.message);
+
+  final String message;
+
+  @override
+  List<Object?> get props => [message];
+}
