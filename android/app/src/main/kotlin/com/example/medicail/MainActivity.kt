070e6ce87f68cc0bcdd3efced62834d5c667d7da
Setsudan <cryptiareta@gmail.com>
2026-08-20 14:18:24 +0200

    test release prepared

--- android/app/src/main/kotlin/com/example/medicail/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/medicail/MainActivity.kt
@@ -2,11 +2,17 @@ package dev.nf2.medicail
 
 import android.os.Bundle
 import androidx.core.view.WindowCompat
+import com.google.firebase.appdistribution.FirebaseAppDistribution
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        FirebaseAppDistribution.getInstance().updateIfNewReleaseAvailable()
     }
 }
